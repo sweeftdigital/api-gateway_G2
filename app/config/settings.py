d1cdea48aa6ede1af2b15c9df41ff0f528bd5947
natatchikhinashvili <153913348+natatchikhinashvili@users.noreply.github.com>
2024-08-24 15:57:18 +0400

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,5 +1,5 @@
 import os
 
 MICROSERVICES = {
-    "accounts": f"accounts:{os.environ.get('ACCOUNT_SERVICE_PORT')}",
+    "accounts": f"accounts-g2-dev-ud32gcxh6a-uc.a.run.app:{os.environ.get('ACCOUNT_SERVICE_PORT')}",
 }

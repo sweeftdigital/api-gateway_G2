4fea5d6469a86b7c61600bfa75f640fe2ae95f82
natatchikhinashvili <153913348+natatchikhinashvili@users.noreply.github.com>
2024-08-30 19:08:59 +0400

    Update helpers.py

--- app/helpers.py
+++ app/helpers.py
@@ -1,13 +1,11 @@
 import httpx
 from starlette.requests import Request
 
-API_START_POINT = "/api/v1"
-
 
 def fill_paths(paths: dict, service: str) -> dict:
     new_paths = {}
     for path, value in paths.items():
-        new_key = f"{API_START_POINT}/{service}/{path.lstrip('/')}"
+        new_key = f"/{service}/{path.lstrip('/')}"
         new_paths[new_key] = value
 
         for method in new_paths[new_key]:
